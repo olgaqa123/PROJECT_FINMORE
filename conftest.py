15d5b2d2186b6bba3e933fae8efb1d945b224479
unknown <gokunolga@gmail.com>
2026-04-16 21:06:17 +0300

    Added files8

--- conftest.py
+++ conftest.py
@@ -1,17 +1,18 @@
 import pytest
+from requests import options
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
  
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--start-maximized")
  
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    driver = webdriver.Chrome(options=options)
  
     yield driver
     driver.quit()
